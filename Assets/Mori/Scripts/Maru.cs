5d44963df2aca23215eabed1bf906e1f7cd03290
morimika <ttc22c1020048@edu.tech.ac.jp>
2023-12-16 15:40:41 +0900

    敵：マルモルのスクリプト完成、プレハブ化

--- Assets/Mori/Scripts/Maru.cs
+++ Assets/Mori/Scripts/Maru.cs
@@ -2,15 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//まずい匂いの時プレイヤーをスルーする処理
+//レイヤーで判定、Odorはすり抜ける
+//gameObject.layer = LayerMask.NameToLayer("Odor");
+
+/// <summary>
+/// マルモルの歩行、追跡のスクリプト
+/// </summary>
 public class Maru : MonoBehaviour
 {
-    // 追いかけたいgameobject
+    //追いかけたいgameobject
     [SerializeField,Header("プレイヤー")]
     private GameObject _player;
-    // 通常歩行スピード
+    //通常歩行スピード
     [SerializeField,Header("通常時の歩行スピード")]
     private float _maruspeed = 2;
-    // 追いかけるスピード
+    //通常歩行範囲
+    [SerializeField, Header("通常時の歩行範囲")]
+    private float _maruArea = 5;
+    //歩行用、増加させて_maruAreaの値になったら引き返す
+    private float _maruAreaAdd = 0;
+    //追いかけるスピード
     [SerializeField,Header("プレイヤー発見時の歩行スピード")]
     private float _angrySpeed = 5f;
     //プレイヤーが索敵範囲内にいるか：全体
@@ -20,70 +32,110 @@ public class Maru : MonoBehaviour
     [Header("前方索敵範囲内にプレイヤーがいるか、居る：true")]
     public bool _playerFrontHere = false;
 
-    //自身のCollider2D
-    [SerializeField]
-    private CircleCollider2D _maruCol;
+    //向きの変更
+    private int _direction = 1;
+    private Vector3 _scare;
 
     private void Start()
     {
-        _maruCol = this.GetComponent<CircleCollider2D>();
+        //初期設定：向き
+        _scare = this.transform.localScale;
     }
-
+    
+    /// <summary>
+    /// 匂いや索敵範囲で判定し追跡または移動する
+    /// </summary>
     private void Update()
     {
-        //美味しそうな匂いのときに実行
         //プレイヤーが索敵範囲内に存在してたら追いかける
+        //・美味しそうな匂いかつ索敵範囲内
+        //・通常時かつ前方索敵範囲内
         if (_player.tag == "Florus" && (_playerHere == true || _playerFrontHere == true)
             || _player.tag == "Player" && _playerFrontHere == true)
         {
-            Debug.Log("Case1:Chase");
-            //コライダーのIsTriggerをFalseにする
-            _maruCol.isTrigger = false;
-            //プレイヤーに向かって移動
-            transform.position
-                = Vector2.MoveTowards(transform.position
-                , _player.transform.position
-                , _angrySpeed * Time.deltaTime);
+            ChaseCase();
         }
-
-        //不味そうな匂い状フラグがTrueのときに毎フレーム実行
-        else if (_player.tag == "Odor")
-        {
-            Debug.Log("Case2:Odor");
-            //コライダーのIsTriggerをTrueにする
-            _maruCol.isTrigger = false;
-            //基本歩行
-            var pos = transform.position;
-
-            //敵の左右移動
-            transform.Translate(pos * _maruspeed * Time.deltaTime);
-            if (pos.x > 1.5)
-            {
-                this.transform.position = new Vector3(-1f, this.transform.position.y, 0);
-            }
-            if (pos.x < -1.5)
-            {
-                this.transform.position = new Vector3(1f, this.transform.position.y, 0);
-            }
-        }
+        //通常時、まずそうな匂い、それ以外
         else
         {
-            Debug.Log("Case3:Normal");
-            //コライダーのIsTriggerをFalseにする
-            _maruCol.isTrigger = false;
-            //基本歩行
-            var pos = transform.position;
+            NonCase();
+        }
 
-            //敵の左右移動
-            transform.Translate(pos * _maruspeed * Time.deltaTime);
-            if (pos.x > 1.5)
+        Debug.Log("プレイヤーが近くにいる : " + _playerHere +" " + "プレイヤーが前にいる : " + _playerFrontHere);
+    }
+
+    /// <summary>
+    /// 追尾歩行、プレイヤーを追いかける
+    /// </summary>
+    private void ChaseCase()
+    {
+        Debug.Log("Case1:Chase");
+
+        //プレイヤーに向かって移動
+        transform.position
+            = Vector2.MoveTowards(this.transform.position
+            , _player.transform.position
+            , _angrySpeed * Time.deltaTime);
+        //もしプレイヤー.xが自分より+だったら
+        if(this.transform.position.x < _player.transform.position.x)
+        {
+            //右向き
+            _scare.x = -1;
+            this.transform.localScale = _scare;
+        }
+        //もしプレイヤー.xが自分より-だったら
+        else
+        {
+            //左向き
+            _scare.x = 1;
+            this.transform.localScale = _scare;
+        }
+    }
+
+    /// <summary>
+    /// 通常歩行
+    /// </summary>
+    private void NonCase()
+    {
+        Debug.Log("Case2:NotFind");
+        //レイヤーで判定、Odorはすり抜ける
+        //gameObject.layer = LayerMask.NameToLayer("Odor");
+
+        //基本歩行
+        //変数に値を入れ続ける
+        _maruAreaAdd += Time.deltaTime * 1.4f;
+        //変数が一定値に達したらリセット、向き反転
+        if(_maruAreaAdd >= _maruArea)
+        {
+            _maruAreaAdd = 0;
+            _direction *= -1;
+            //歩行向きが+なら右向き
+            if(_direction == 1)
             {
-                this.transform.position = new Vector3(-1f, 0, 0);
+                _scare.x = -1;
+                this.transform.localScale = _scare;
             }
-            if (pos.x < -1.5)
+            //歩行向きが-なら左向き
+            else
             {
-                this.transform.position = new Vector3(1f, 0, 0);
+                _scare.x = 1;
+                this.transform.localScale = _scare;
             }
         }
+        //歩き続ける
+        transform.position 
+            = new Vector3(transform.position.x + _maruspeed * Time.fixedDeltaTime * _direction
+            , this.transform.position.y
+            , 0);
+    }
+
+    /// <summary>
+    /// デバッグ用
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player") Debug.LogWarning("Debug : Hit,OK");
+        if (collision.collider.tag == "Odor") Debug.LogError("Debug : Hit,ERROR");
     }
 }
